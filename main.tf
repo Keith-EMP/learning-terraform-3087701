a6f0655e15d843cd48cff57c70dee8517b021f5f
Keith Akins <129796505+Keith-EMP@users.noreply.github.com>
2025-01-17 15:56:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-
+  subnet_id = "subnet-dcdff3f7"
   tags = {
     Name = "HelloWorld"
   }
