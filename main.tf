f4655fa0c17199258e46c0448ab044ebcab2ccb4
Keith Akins <129796505+Keith-EMP@users.noreply.github.com>
2025-01-27 12:02:59 -0600

    ?

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "blog_alb" {
   load_balancer_type = "application"
 
   vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  subnets            = [module.blog_vpc.public_subnets[0],module.blog_vpc.public_subnets[1]]
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
